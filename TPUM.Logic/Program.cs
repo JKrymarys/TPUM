2d6c8d3d7c0bb094be680b1d89c29d8700b90e4b
Jakub Krymarys <krymarys.jakub@gmail.com>
2020-04-05 13:57:58 +0200

    Create Projects for Data and Logic layer. Add test connection between them.

---
+++ TPUM.Logic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TPUM.Data;
+
+namespace TPUM.Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tc = new TestConnection();
+           int randomNumber =  tc.generateRandomNumber();
+            Console.WriteLine("Hello World! " + randomNumber);
+        }
+    }
+}
